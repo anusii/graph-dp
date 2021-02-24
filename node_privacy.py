a2a9fee111b30e21d8c06f3ef1021bebfe9421fb
Michael Purcell <Michael.Purcell1@anu.edu.au>
2021-02-24 15:58:46 +1100

    Added linear programming Lipschitz extension stuff

--- node_privacy.py
+++ node_privacy.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import networkx as nx
 
@@ -7,36 +6,49 @@ import copy
 from relm.mechanisms import LaplaceMechanism, CauchyMechanism
 
 # =============================================================================
+# Convex Optimization Approach
+# =============================================================================
+
+# =============================================================================
 # Edge Counts
 # =============================================================================
 def flow_graph(G, D):
-    V_left = list(zip(['left']*len(G), G.nodes()))
-    V_right = list(zip(['right']*len(G), G.nodes()))
+    V_left = list(zip(["left"] * len(G), G.nodes()))
+    V_right = list(zip(["right"] * len(G), G.nodes()))
     F = nx.DiGraph()
     F.add_nodes_from(V_left)
     F.add_nodes_from(V_right)
-    F.add_nodes_from('st')
-    F.add_weighted_edges_from([('s', vl, D) for vl in V_left], weight="capacity")
-    F.add_weighted_edges_from([(vr, 't', D) for vr in V_right], weight="capacity")
-    F.add_weighted_edges_from([(('left', u), ('right', v), 1) for u,v in G.edges()], weight="capacity")
-    F.add_weighted_edges_from([(('right', u), ('left', v), 1) for u,v in G.edges()], weight="capacity")
+    F.add_nodes_from("st")
+    F.add_weighted_edges_from([("s", vl, D) for vl in V_left], weight="capacity")
+    F.add_weighted_edges_from([(vr, "t", D) for vr in V_right], weight="capacity")
+    F.add_weighted_edges_from(
+        [(("left", u), ("right", v), 1) for u, v in G.edges()], weight="capacity"
+    )
+    F.add_weighted_edges_from(
+        [(("right", u), ("left", v), 1) for u, v in G.edges()], weight="capacity"
+    )
     return F
 
 
-n = 2**8
-D = 50
+n = 2 ** 8
+D = 2 ** 7
 p = 0.25
 G = nx.random_graphs.gnp_random_graph(n, p)
 print(max(dict(G.degree()).values()))
 
 F = flow_graph(G, D)
 
-foo = nx.minimum_cut_value(F, _s='s', _t='t')
+foo = nx.minimum_cut_value(F, _s="s", _t="t")
+
+print(foo)
+print(len(G.edges()))
 
 # =============================================================================
-
+# Linear functions of the degree sequence with concave base function h
+# =============================================================================
 class Edge(object):
     """ An undirected, capacity limited edge. """
+
     def __init__(self, capacity):
         self.capacity = capacity
         self.flow = cp.Variable()
@@ -53,6 +65,7 @@ class Edge(object):
 
 class Node(object):
     """ A node with accumulation. """
+
     def __init__(self, accumulation=0):
         self.accumulation = accumulation
         self.edge_flows = []
@@ -61,9 +74,10 @@ class Node(object):
     def constraints(self):
         return [sum(f for f in self.edge_flows) == self.accumulation]
 
-n = 2**6
-D = 2**5
-p = 2**-2
+
+n = np.random.randint(2 ** 6, 2 ** 7)
+D = 2 ** 5
+p = 2 ** -2
 G = nx.random_graphs.gnp_random_graph(n, p)
 
 left_nodes = [Node() for node in G.nodes()]
@@ -85,7 +99,7 @@ for rn in right_nodes:
     target_edges.append(edge)
 
 inter_edges = []
-for u,v in G.edges():
+for u, v in G.edges():
     edge = Edge(capacity=1.0)
     edge.connect(left_nodes[u], right_nodes[v])
     inter_edges.append(edge)
@@ -96,24 +110,27 @@ for u,v in G.edges():
 edges = source_edges + target_edges + inter_edges
 
 constraints = []
-for obj in  nodes + edges:
+for obj in nodes + edges:
     constraints += obj.constraints()
 
 x = np.arange(n)
-k = 1
-y = scipy.special.comb(x, k) / 2
+# y = x / 2.0
+y = np.ones(x.shape)
+
 
 def h1(x):
     x_low = cp.floor(x)
     x_high = cp.ceil(x)
     return y[int(x_low.value)] * (x - x_low) + y[int(x_high.value)] * (x_high - x)
 
+
 def h2(x):
     m = y[1:] - y[:-1]
-    x0 = np.arange(n-1)
+    x0 = np.arange(n - 1)
     y0 = y[:-1]
-    z = y0 + cp.multiply(m, (x-x0))
+    z = y0 + cp.multiply(m, (x - x0))
     return cp.min(z)
+
 
 def h(x):
     try:
@@ -122,7 +139,42 @@ def h(x):
         ret = h2(x)
     return ret
 
+
 prob = cp.Problem(cp.Maximize(cp.sum([h(e.flow) for e in source_edges])), constraints)
-%time value = prob.solve()
+value = prob.solve()
 
 print(value)
+# print(len(G.edges()))
+print(len(G.nodes()))
+
+# =============================================================================
+# Linear programming approach
+# =============================================================================
+n = np.random.randint(2 ** 4, 2 ** 5)
+D = 2 ** 2
+p = 2 ** -2
+G = nx.random_graphs.gnp_random_graph(n, p)
+
+k = 3
+triangles = nx.triads_by_type(nx.DiGraph(G))["300"]
+print(D)
+print(max(dict(G.degree).values()))
+
+
+print(k * D * (D - 1) ** (k - 2))
+print(max(nx.triangles(G).values()))
+
+c = -np.ones(len(triangles))
+A_ub = np.zeros((n, len(triangles)))
+for i, t in enumerate(triangles):
+    nodes = triangles[i].nodes()
+    for node in nodes:
+        A_ub[node, i] = 1
+
+b_ub = np.ones(n) * k * D * (D - 1) ** (k - 2)
+
+bounds = (0.0, 1.0)
+
+res = scipy.optimize.linprog(c, A_ub=A_ub, b_ub=b_ub, bounds=bounds)
+print(len(triangles))
+print(-res.fun)
